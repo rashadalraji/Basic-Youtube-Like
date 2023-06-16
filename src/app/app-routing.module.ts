f5f2d67b5f07a554959ccbeebafdf53993e2e2cc
rashadalraji <rashadalraji@gmail.com>
2023-06-16 18:34:54 +0600

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideoComponent } from './features/video/video.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+  {path:'home', component: VideoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
