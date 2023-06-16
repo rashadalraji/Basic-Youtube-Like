57a6129a8309f46ed19bbe0fdfd513cede49dc0b
rashadalraji <rashadalraji@gmail.com>
2023-06-16 17:26:56 +0600

    angular test update

--- src/app/features/video/video.component.ts
+++ src/app/features/video/video.component.ts
@@ -17,7 +17,14 @@ export class VideoComponent implements OnInit {
 
   }
 
-  videoList: any
+  videoList: any;
+  suggestionList: any;
+  isVideoAvailable: boolean = true;
+  isSmallVideoAvailable: boolean = false;
+  isVideoClicked: boolean = false;
+  isSmallVideoClicked: boolean = false;
+  search : any = '';
+  suggestionListBackUp : any;
 
   ngOnInit() {
     this.getVideList()
@@ -28,22 +35,33 @@ export class VideoComponent implements OnInit {
     this.commonService.getVideoList().subscribe((res: any) => {
       if (res) {
         this.videoList = res;
+        this.suggestionList = [...this.videoList];
+        this.suggestionListBackUp = [...this.videoList];
+        this.isVideoAvailable = true;
+        this.isSmallVideoAvailable = true;
+      }else{
+        this.isVideoAvailable = false;
       }
     })
   }
 
+  onClickHomeBtn(){
+    this.isVideoClicked = ! this.isVideoClicked;
+    this.getVideList()
+  }
+
 
   pauseVideo(videoplayer: any) {
-    videoplayer.nativeElement.play();
-    // this.startedPlay = true;
-    // if(this.startedPlay == true)
-    // {
-    setTimeout(() => {
-      videoplayer.nativeElement.pause();
+    videoplayer.nativeElement.play();    
+    // setTimeout(() => {
+      
       if (videoplayer.nativeElement.paused) {
         this.show = !this.show;
       }
-    }, 5000);
+      if(this.show){
+        videoplayer.nativeElement.pause();
+      }
+    // }, 5000);
     // }
   }
 
@@ -51,14 +69,77 @@ export class VideoComponent implements OnInit {
     this.show = !this.show;
     videoplayer.nativeElement.play();
   }
+  // listName:string = 'videoList'
 
-  search : any = '';
-  onClickSearchBtn() {
-    if (this.search) {
-      this.videoList = this.videoList.filter((el: any) => el.title.toLowerCase().includes(this.search.toLowerCase()))
-    } else {
-      this.getVideList()
-    }
+  onClickSearchBtn(listName:string = 'videoList') {
+   
+    setTimeout(()=>{
+      if (this.search) {
+        if(listName == 'videoList'){
+          this.videoList = this.videoList.filter((el: any) => el.title.toLowerCase().includes(this.search.toLowerCase()));
+        if(this.videoList && this.videoList.length > 0){
+          this.isVideoAvailable = true;
+        }else{
+          this.isVideoAvailable = false;
+        }
+      }else{
+
+        this.suggestionList = this.suggestionList.filter((el: any) => el.title.toLowerCase().includes(this.search.toLowerCase()));
+        if(this.suggestionList && this.suggestionList.length > 0){
+          this.isSmallVideoAvailable = true;
+        }else{
+          this.isSmallVideoAvailable = false;
+        }
+      }
+      } else {
+        if(listName == 'videoList'){
+          this.getVideList()
+        }else{          
+          this.suggestionList = [...this.suggestionListBackUp]
+          this.isSmallVideoAvailable = true;
+        }
+        
+      }
+    },1000)
+   
   }
+
+  onClickVideoInfo(id:string){
+    this.isVideoClicked = true;
+    this.isSmallVideoClicked = false;
+    let index = this.suggestionList.findIndex((el:any)=>el.id==id);
+    let masterIndex = this.videoList.findIndex((el:any)=>el.id==id);
+    if(index >= 0){
+      this.suggestionList.splice(index,1);
+      // this.videoList = this.videoList.slice(masterIndex,1);
+    }
+    if(masterIndex >= 0){    
+      this.videoList = this.videoList.slice(masterIndex,masterIndex+1);
+      
+    }
+    
+  }
+
+
+  onClickSmallVideoInfo(data:any){
+    this.isSmallVideoClicked = true;
+    // this.isVideoClicked = false;
+
+    
+    let tempData = data;
+    let index = this.suggestionList.findIndex((el:any)=>el.id==data.id);
+    
+    if(index >= 0){
+      this.suggestionList.splice(index,1,this.videoList[0]);
+      this.suggestionListBackUp.splice(index,1,this.videoList[0]);
+      
+    }
+
+    this.videoList[0] = tempData;
+   
+    
+  }
+
+ 
 
 }
