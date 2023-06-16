4fa677bfed07996c62c45aa4ed4b0278d5008e47
rashadalraji <rashadalraji@gmail.com>
2023-06-16 13:12:43 +0600

    search functionalty added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -21,7 +21,7 @@ import { VideoComponent } from './features/video/video.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-  
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/video/video.component.ts
+++ src/app/features/video/video.component.ts
@@ -9,45 +9,56 @@ import { CommonService } from 'src/app/services/common.service';
   styleUrls: ['./video.component.css']
 })
 export class VideoComponent implements OnInit {
+  @ViewChild('videoPlayer') videoplayer: any;
+  public startedPlay: boolean = false;
+  public show: boolean = false;
 
-  constructor(private commonService : CommonService){
+  constructor(private commonService: CommonService) {
 
   }
 
-  videoList : any[] =[]
+  videoList: any
 
-  ngOnInit(){
-    console.log('inIt')
-    this.commonService.getVideoList().subscribe((res:any)=>{
-      if(res){
+  ngOnInit() {
+    this.getVideList()
+    
+  }
+
+  getVideList(){
+    this.commonService.getVideoList().subscribe((res: any) => {
+      if (res) {
         this.videoList = res;
       }
     })
   }
 
-  @ViewChild('videoPlayer') videoplayer: any;
-public startedPlay:boolean = false;
-public show:boolean = false;
-pauseVideo(videoplayer:any)
-{
-  videoplayer.nativeElement.play();
-  // this.startedPlay = true;
-  // if(this.startedPlay == true)
-  // {
-     setTimeout(() => 
-     {
-      videoplayer.nativeElement.pause();
-       if(videoplayer.nativeElement.paused)
-      {
-        this.show = !this.show;       
-      } 
-     }, 5000);
-  // }
-}
 
-closebutton(videoplayer:any){
-  this.show = !this.show; 
-  videoplayer.nativeElement.play();
-}
+  pauseVideo(videoplayer: any) {
+    videoplayer.nativeElement.play();
+    // this.startedPlay = true;
+    // if(this.startedPlay == true)
+    // {
+    setTimeout(() => {
+      videoplayer.nativeElement.pause();
+      if (videoplayer.nativeElement.paused) {
+        this.show = !this.show;
+      }
+    }, 5000);
+    // }
+  }
+
+  closebutton(videoplayer: any) {
+    this.show = !this.show;
+    videoplayer.nativeElement.play();
+  }
+
+  search : any = '';
+  onClickSearchBtn() {
+    if (this.search) {
+      this.videoList = this.videoList.filter((el: any) => el.title.toLowerCase().includes(this.search.toLowerCase()))
+    } else {
+      this.getVideList()
+    }
+  }
 
 }
