d5b45a21823058146754ed3e184b3e9ccbcb24b3
rashadalraji <rashadalraji@gmail.com>
2023-06-17 13:34:59 +0600

    complete project

--- src/app/features/video/video.component.ts
+++ src/app/features/video/video.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonService } from 'src/app/services/common.service';
 
-
-
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
@@ -12,29 +10,25 @@ export class VideoComponent implements OnInit {
   @ViewChild('videoPlayer') videoplayer: any;
   public startedPlay: boolean = false;
   public show: boolean = false;
-
-  constructor(private commonService: CommonService) {
-
-  }
-
   videoList: any;
   videoListBackUp: any;
   suggestionList: any;
-  suggestionListBackUp : any;
+  suggestionListBackUp: any;
   isVideoAvailable: boolean = true;
   isSmallVideoAvailable: boolean = false;
   isVideoClicked: boolean = false;
   isSmallVideoClicked: boolean = false;
-  search : any = '';
- 
+  search: any = '';
+
+  constructor(private commonService: CommonService) { }
 
   ngOnInit() {
     this.getVideList()
-    
+
   }
 
   // fetch data from API
-  getVideList(){
+  getVideList() {
     this.commonService.getVideoList().subscribe((res: any) => {
       if (res) {
         this.videoList = res;
@@ -43,30 +37,30 @@ export class VideoComponent implements OnInit {
         this.videoListBackUp = [...this.videoList];
         this.isVideoAvailable = true;
         this.isSmallVideoAvailable = true;
-      }else{
+      } else {
         this.isVideoAvailable = false;
       }
     })
   }
 
   // Returning to home 
-  onClickHomeBtn(){
-    this.isVideoClicked = ! this.isVideoClicked;
+  onClickHomeBtn() {
+    this.isVideoClicked = !this.isVideoClicked;
     this.getVideList()
   }
 
 
   // video player related
   pauseVideo(videoplayer: any) {
-    videoplayer.nativeElement.play();    
+    videoplayer.nativeElement.play();
     // setTimeout(() => {
-      
-      if (videoplayer.nativeElement.paused) {
-        this.show = !this.show;
-      }
-      if(this.show){
-        videoplayer.nativeElement.pause();
-      }
+
+    if (videoplayer.nativeElement.paused) {
+      this.show = !this.show;
+    }
+    if (this.show) {
+      videoplayer.nativeElement.pause();
+    }
     // }, 5000);
     // }
   }
@@ -77,93 +71,93 @@ export class VideoComponent implements OnInit {
   }
 
   // video player end
-  
+
 
   // search functionality
-  onClickSearchBtn(listName:string) {
-   
-    setTimeout(()=>{
-      if (this.search) {
-        if(listName == 'videoList'){
-          this.videoList = this.videoList.filter((el: any) => el.title.toLowerCase().includes(this.search.toLowerCase()));
-        if(this.videoList && this.videoList.length > 0){
-          this.isVideoAvailable = true;
-        }else{
-          this.isVideoAvailable = false;
-        }
-      }else{
+  onClickSearchBtn(listName: string) {
 
-        this.suggestionList = this.suggestionListBackUp.filter((el: any) => el.title.toLowerCase().includes(this.search.toLowerCase()));
-        if(this.suggestionList && this.suggestionList.length > 0){
-          this.isSmallVideoAvailable = true;
-        }else{
-          this.isSmallVideoAvailable = false;
+    setTimeout(() => {
+      if (this.search) {
+        if (listName == 'videoList') {
+          this.videoList = this.videoList.filter((el: any) => el.title.toLowerCase().includes(this.search.toLowerCase()));
+          if (this.videoList && this.videoList.length > 0) {
+            this.isVideoAvailable = true;
+          } else {
+            this.isVideoAvailable = false;
+          }
+        } else {
+
+          this.suggestionList = this.suggestionListBackUp.filter((el: any) => el.title.toLowerCase().includes(this.search.toLowerCase()));
+          if (this.suggestionList && this.suggestionList.length > 0) {
+            this.isSmallVideoAvailable = true;
+          } else {
+            this.isSmallVideoAvailable = false;
+          }
         }
-      }
       } else {
-        if(listName == 'videoList'){
+        if (listName == 'videoList') {
           this.getVideList()
-        }else{          
+        } else {
           this.suggestionList = [...this.suggestionListBackUp]
           this.isSmallVideoAvailable = true;
         }
-        
+
       }
-    },1000)
-   
+    }, 1000)
+
   }
 
   // Master video click function
-  onClickVideoInfo(id:string){
+  onClickVideoInfo(id: string) {
     this.isVideoClicked = true;
     this.isSmallVideoClicked = false;
     this.generateRandomSuggestionList();
-    let index = this.suggestionList.findIndex((el:any)=>el.id==id);
-    let masterIndex = this.videoList.findIndex((el:any)=>el.id==id);
-    if(index >= 0){
-      this.suggestionList.splice(index,1);     
+    let index = this.suggestionList.findIndex((el: any) => el.id == id);
+    let masterIndex = this.videoList.findIndex((el: any) => el.id == id);
+    if (index >= 0) {
+      this.suggestionList.splice(index, 1);
     }
-    if(masterIndex >= 0){    
-      this.videoList = this.videoList.slice(masterIndex,masterIndex+1);
-      
+    if (masterIndex >= 0) {
+      this.videoList = this.videoList.slice(masterIndex, masterIndex + 1);
+
     }
-    
+
   }
 
 
 
-  onClickSuggestedVideo(data:any){
-    this.isSmallVideoClicked = true;  
-    let tempData = data;    
-    let index = this.suggestionList.findIndex((el:any)=>el.id==tempData.id);
+  onClickSuggestedVideo(data: any) {
+    this.isSmallVideoClicked = true;
+    let tempData = data;
+    let index = this.suggestionList.findIndex((el: any) => el.id == tempData.id);
 
-    this.generateRandomSuggestionList();    
-    if(index >= 0){
-      this.suggestionList.splice(index,1,this.videoList[0]);
-      this.suggestionListBackUp.splice(index,1,this.videoList[0]);      
+    this.generateRandomSuggestionList();
+    if (index >= 0) {
+      this.suggestionList.splice(index, 1, this.videoList[0]);
+      this.suggestionListBackUp.splice(index, 1, this.videoList[0]);
     }
-    this.videoList[0] = tempData;  
-    
+    this.videoList[0] = tempData;
+
   }
 
   // Generating rando video ass suggestion for user
-  generateRandomSuggestionList(){
-    let randomList : any = [];
+  generateRandomSuggestionList() {
+    let randomList: any = [];
     let totalVideoCount = this.videoListBackUp.length;
-    for(var i = 0; i < 5; i++){
-      let randomIndex = Math.floor(Math.random() * (totalVideoCount-1));
+    for (var i = 0; i < 5; i++) {
+      let randomIndex = Math.floor(Math.random() * (totalVideoCount - 1));
       randomList.push(randomIndex);
     }
-    let removeDuplicates : any = [... new Set(randomList)];
+    let removeDuplicates: any = [... new Set(randomList)];
     let len = removeDuplicates.length;
     this.suggestionList = [];
-    for(var j = 0; j < len; j++){
-      this.suggestionList.push(this.suggestionListBackUp[removeDuplicates[j]]);      
+    for (var j = 0; j < len; j++) {
+      this.suggestionList.push(this.suggestionListBackUp[removeDuplicates[j]]);
     }
 
     return this.suggestionList;
   }
 
- 
+
 
 }
